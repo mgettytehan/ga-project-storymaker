1b40905ccb383acdbd52fc58f9958dff9c4e851d
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-09-17 11:24:15 -0400

    Added bare bones of story display page with working title, text display and preliminary (but nonfunctional) choice links

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import StoryDisplay from './components/StoryDisplay.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          {/* <Route exact path="/" component={HelloWorld}/> */}
-        </Switch>
-      </Router>
+      <StoryDisplay />
     </div>
   );
 }
--- client/src/components/StoryDisplay.js
+++ client/src/components/StoryDisplay.js
@@ -1,11 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+const storyLink = (choice) => {
+    //current contains dummy link instead of choice action
+    return (<p><a href="#">{choice.choiceText}</a></p>);
+}
+
+const storyText = (text) => {
+    return (<p>{text}</p>);
+}
+
+const storyTextDisplay = (text, choices) => {
+    return (
+        <div>
+            {storyText(text)}
+            {choices.map(storyLink)}
+        </div>
+    );
+}
+
+const storyDetails = (storyTitle) => {
+    //add in author when users are introduced
+    return (<h1>{storyTitle}</h1>);
+}
 
 export default class StoryDisplay extends Component {
+    //state hardcoded for testing
     state = {
+        story: {
+            title: "Hardcoded Test Story",
+            firstNodeId: "test123"
+        },
+        currentNode: {
+            storyId: "test123",
+            nodeTitle: "The Start",
+            storyText: "And so it begins.",
+            choices: [
+                {
+                    choiceText: "Fly the Excalibur",
+                    nextNode: "test124"
+                },
+                {
+                    choiceText: "Give up on curing the Drakh plague",
+                    nextNode: "test125"
+                }
+            ]
+        }
     }
 
     render() {
         return (
-        )
+            <div>
+                {storyDetails( this.state.story.title )}
+                {storyTextDisplay(
+                    this.state.currentNode.storyText
+                    , this.state.currentNode.choices
+                )}
+            </div>
+        );
     }
 }
