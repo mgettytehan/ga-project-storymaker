e2412a4bab98406cb72036f77b322871fecfe893
Marion Getty-Tehan <mgettytehan@gmail.com>
2019-09-19 10:48:25 -0400

    Added bones of NewStory form

--- client/src/components/NewStory.js
+++ client/src/components/NewStory.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
+
 export default class NewStory extends Component {
     state = {
-
+        title: ""
     }
     render() {
+        return (<form>
+            <label>Story title:</label>
+            <input type="text" />
+            <input type="submit" />
+        </form>)
     }
 }
